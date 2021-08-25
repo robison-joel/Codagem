c9ec35866365495d7c6e9301b6e3f8d761c60a7c
robison-joel <64526672+robison-joel@users.noreply.github.com>
2021-08-25 05:54:09 -0300

    Add files via upload

---
+++ JavaScript/condicao_composta.js
@@ -0,0 +1,8 @@
+//
+var pais = 'Brasil'
+console.log(`Você vive no/em: ${pais}`)
+if (pais == 'Brasil') {
+    console.log(`Tú és Brasileiro.`)
+} else {
+    console.log(`Tú é Gringo...rápa fora!`)
+}
