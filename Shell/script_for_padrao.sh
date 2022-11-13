5de5636636afebe75d32cb222ce8f9841caa5b72
Róbison Joel Guimarães Simon <64526672+robison-joel@users.noreply.github.com>
2022-11-13 00:38:39 -0300

    Add files via upload

---
+++ Shell/script_for_padrao.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+#
+#######################################
+# NOME...: Robison Joel
+# DATA...: 05/11/2022
+# ASSUNTO: Script de loop padrão
+#######################################
+#
+
+#
+for i in (cat file_1.txt); do 
+
+    echo $i
+
+done
